3e72af846e2d4623068bc41902ce5864214c83e6
Éric Pinto <ericgrandopinto@gmail.com>
2021-01-26 20:06:07 -0300

    first commit

---
+++ api-publications/src/main/java/com/ericpinto/publicationsapp/ApiPublicationsApplication.java
@@ -0,0 +1,13 @@
+package com.ericpinto.publicationsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPublicationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPublicationsApplication.class, args);
+    }
+
+}
